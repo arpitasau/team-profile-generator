a05aaf3a57211898bd4aa87eebf432b516dc311a
Arpita K Sau <arpitakaran@gmail.com>
2020-07-18 00:54:32 -0500

    updated-app.js

--- app.js
+++ app.js
@@ -13,23 +13,84 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const team = [];
+const choices = ['Manager', 'Engineer', 'Intern', '-Done-'];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const questions = [
+    {
+        type: 'list',
+        message: 'Add Employee as-',
+        name: 'type',
+        choices: choices
+    },
+    {
+        type: 'input',
+        message: "Enter Employee's Name:",
+        name: 'name',
+        default: 'First Name Last Name',
+        when: response => response.type !== '-Done-',
+        validate: response => (response.length > 0) || "Name can't be blank"
+    },
+    {
+        type: 'input',
+        message: 'Enter employee ID:',
+        name: 'id',
+        when: response => response.type !== '-Done-',
+        validate: response => (parseInt(response) > 0) || 'Please enter a valid ID'
+    },
+    {
+        type: 'input',
+        message: "Enter employee's email:",
+        name: 'email',
+        default: 'email@email.com',
+        when: response => response.type !== '-Done-',
+        validate: response => (/\S+@\S+\.\S+/.test(response)) || 'Please enter a valid email'
+    },
+    {
+        type: 'input',
+        message: 'Enter office number:',
+        name: 'office',
+        when: response => response.type !== '-Done-' && response.type === 'Manager',
+        validate: response => (parseInt(response) > 0) || 'Please enter a valid number'
+    },
+    {
+        type: 'input',
+        message: 'Enter GitHub username:',
+        name: 'github',
+        default: 'Username',
+        when: response => response.type !== '-Done-' && response.type === 'Engineer',
+        validate: response => (response.length > 0) || "Name can't be blank"
+    },
+    {
+        type: 'input',
+        message: 'Enter school name:',
+        name: 'school',
+        default: 'School Name',
+        when: response => response.type !== '-Done-' && response.type === 'Intern',
+        validate: response => (response.length > 0) || "School name can't be blank"
+    }
+];
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+// function to prompt user
+const prompt = () => inquirer.prompt(questions).then(response => {
+    if (response.type !== '-Done-') {
+        if (response.type === 'Manager') {
+            team.push(new Manager(response.name, response.id, response.email, response.office));
+            choices.splice(0, 1)
+        } else if (response.type === 'Engineer') {
+            team.push(new Engineer(response.name, response.id, response.email, response.github));
+        } else if (response.type === 'Intern') {
+            team.push(new Intern(response.name, response.id, response.email, response.school));
+        }
+        prompt();
+        // render the team html and output to a file
+    } else {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR);
+        }
+        fs.writeFile(outputPath, render(team), err => err ? console.log(err) : console.log(`Generated ${outputPath}`));
+    }
+});
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// calling function prompt
+prompt();
